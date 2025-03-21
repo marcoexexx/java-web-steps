4bd8ba5494c8784b61f586d9475954ebd3b4b167
Aung Koko Lwin <toyko2001@gmail.com>
2025-03-21 14:11:54 +0630

    fix: servlet init

--- my-jsp/src/main/resources/com/example/Hello.java
+++ my-jsp/src/main/resources/com/example/Hello.java
@@ -7,13 +7,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(urlPatterns = "/helloi")
 public class Hello extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+  @Override
+  public void init() throws ServletException {
+    System.out.println("\n\n[ INIT ] SERVER HAVE BEEN INITIALIZED.");
+  }
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("text/html");
-    response.getWriter().println("<h1>Hello, Servlet!</h1>");
+  @Override
+  public void destroy() {
+    System.out.println("\n\n[ DESTROY ] SERVER HAVE BEEN DESTROYED.");
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/plain");
+
+    response.getWriter().println("Rusty");
   }
 }
