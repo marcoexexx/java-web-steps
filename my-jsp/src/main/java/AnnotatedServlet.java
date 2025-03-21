635a5dc1c3e1e0da912a416f288aff339e4da6e5
Aung Koko Lwin <toyko2001@gmail.com>
2025-03-21 15:37:54 +0630

    add: url mapping

--- my-jsp/src/main/java/AnnotatedServlet.java
+++ my-jsp/src/main/java/AnnotatedServlet.java
@@ -1,15 +1,27 @@
 import java.io.IOException;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/some")
+@WebServlet(name = "AnnotatedServlet", urlPatterns = { "/some", "/other" })
 public class AnnotatedServlet extends HttpServlet {
-  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-    System.out.println("HELLO");
+  @Override
+  public void init() throws ServletException {
+    System.out.println("[ INITIALIZE ] server is start");
   }
+
+  @Override
+  public void destroy() {
+    System.out.println("[ DESTROY ] server have been destroyed");
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().write("Rusty");
+  }
+
 }
