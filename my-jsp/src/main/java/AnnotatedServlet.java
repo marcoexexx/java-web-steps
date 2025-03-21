40e7d1603e02aad3c9f84fd96f4fde20ebccd906
Aung Koko Lwin <toyko2001@gmail.com>
2025-03-21 15:10:22 +0630

    setup route

---
+++ my-jsp/src/main/java/AnnotatedServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/some")
+public class AnnotatedServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    System.out.println("HELLO");
+  }
+}
