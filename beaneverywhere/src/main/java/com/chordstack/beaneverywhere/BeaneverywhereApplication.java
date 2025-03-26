49afec62524efb06b7ef6fbaada5002f0a1f0383
Aung Koko Lwin <toyko2001@gmail.com>
2025-03-26 08:35:24 +0630

    fmt

--- beaneverywhere/src/main/java/com/chordstack/beaneverywhere/BeaneverywhereApplication.java
+++ beaneverywhere/src/main/java/com/chordstack/beaneverywhere/BeaneverywhereApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeaneverywhereApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BeaneverywhereApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BeaneverywhereApplication.class, args);
+  }
 }
