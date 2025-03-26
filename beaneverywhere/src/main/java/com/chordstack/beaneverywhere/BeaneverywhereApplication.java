f39f03d9912c6c179ba9a3beeb862cac31828118
Aung Koko Lwin <toyko2001@gmail.com>
2025-03-26 07:54:16 +0630

    init `bean every where` spring boot demo.

---
+++ beaneverywhere/src/main/java/com/chordstack/beaneverywhere/BeaneverywhereApplication.java
@@ -0,0 +1,13 @@
+package com.chordstack.beaneverywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaneverywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaneverywhereApplication.class, args);
+	}
+
+}
---
+++ beaneverywhere/src/test/java/com/chordstack/beaneverywhere/BeaneverywhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chordstack.beaneverywhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaneverywhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
